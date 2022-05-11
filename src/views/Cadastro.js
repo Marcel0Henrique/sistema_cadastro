2291f0f064a7b491b170efa6401d6ea29c0f51f2
Marcelo Henrique <marcelomonteiro1275@gmail.com>
2022-05-11 09:55:43 -0300

    update

--- src/views/Cadastro.js
+++ src/views/Cadastro.js
@@ -1,76 +1,116 @@
 import React from "react";
 import './Cadastro.css';
-
-function salvar() {
-
-    let nome = document.getElementById('input_nome').value;
-    let codigo = document.getElementById('input_codigo').value;
-    let descricao = document.getElementById('textArea_descricao').value;
-    let preco = document.getElementById('input_preco').value;
-    let fornecedor = document.getElementById('input_fornecedor').value;
-
-    if (nome === '' || codigo === '' || descricao === '' || preco === '' || fornecedor === '') {
-
-        alert('Favor preencher todos os campos!');
-
-    } else {
-        let produto = { 'nome': nome, 'codigo': codigo, 'descricao': descricao, 'preco': preco, 'fornecedor': fornecedor };
-        localStorage.setItem(localStorage.length, JSON.stringify(produto));
-
-        limpar();
-    }
-}
-
-function limpar() {
-    document.getElementById('input_nome').value = null;
-    document.getElementById('input_codigo').value = '';
-    document.getElementById('textArea_descricao').value = '';
-    document.getElementById('input_preco').value = '';
-    document.getElementById('input_fornecedor').value = '';
-}
-
-
+import ProdutoService from "../app/produtoService";
 
 
 class Cadastro extends React.Component {
+    state = {
+        nome: '',
+        codigo: '',
+        descricao: '',
+        preco: '',
+        fornecedor: '',
+
+        sucesso: false
+    }
+
+    onChange = (event) => {
+        const valor = event.target.value
+        const nomeDoCampo = event.target.name
+        this.setState({
+            [nomeDoCampo]: valor
+        })
+    }
+
+    limparCampos = (event) => {
+        console.log("teste")
+        this.setState({
+            nome: '',
+            codigo: '',
+            descricao: '',
+            preco: '',
+            fornecedor: '',
+            sucesso: false
+        })
+    }
+
+    onSubmit = (event) => {
+        this.setState({ sucesso: true })
+        const produto = {
+            'nome': this.state.nome,
+            'codigo': this.state.codigo,
+            'descricao': this.state.descricao,
+            'preco': this.state.preco,
+            'fornecedor': this.state.fornecedor,
+        }
+
+        this.service.salvar(produto);
+        this.limpaCampos()
+
+
+    }
+
+
+
+    constructor() {
+        super()
+        this.service = new ProdutoService();
+    }
+
     render() {
         return (
             <div className="container space">
 
-                <div class="card bg-info mb-3">
-                    <div class="card-header text-white text-center">Cadastro de Produtos</div>
-                    <div class="card-body bg-secondary text-black">
+
+
+
+                <div className="card bg-info mb-3">
+                    <div className="card-header text-white text-center">Cadastro de Produtos</div>
+                    <div className="card-body bg-secondary text-black">
+
+                        {
+                            this.state.sucesso ? (
+                                <div className="alert alert-dismissible alert-success">
+                                    <button type="button" className="btn-close" data-bs-dismiss="alert" onClick={() => { this.setState({ sucesso: false }) }}></button>
+
+                                    <strong>O produto {this.state.nome} foi cadastrado com sucesso</strong>.
+                                </div>
+                            ) : (
+                                <></>
+                            )
+                        }
+
 
                         <form>
                             <fieldset>
-                                <div class="row">
-                                    <div class="col">
-                                        <label>Nome <span class='text-red'>*</span> </label>
-                                        <input type="text" id='input_nome' class="form-control" required></input>
+                                <div className="row">
+                                    <div className="col">
+                                        <label>Nome <span className='text-red'>*</span> </label>
+                                        <input type="text" id='input_nome' name="nome" className="form-control" value={this.state.nome} onChange={this.onChange} required></input>
                                     </div>
-                                    <div class="col">
-                                        <label>Código <span class='text-red'>*</span> </label>
-                                        <input type="number" id='input_codigo' class="form-control" required></input>
+                                    <div className="col">
+                                        <label>Código <span className='text-red'>*</span> </label>
+                                        <input type="number" id='input_codigo' name="codigo" className="form-control" value={this.state.codigo} onChange={this.onChange} required></input>
                                     </div>
                                 </div>
 
                                 <br></br>
 
-                                <div class="form-group">
-                                    <label>Descrição <span class='text-red'>*</span> </label>
-                                    <textarea class="form-control text_area" id="textArea_descricao" rows="6" required></textarea>
+                                <div className="form-group">
+                                    <label>Descrição <span className='text-red'>*</span> </label>
+                                    <textarea className="form-control text_area" name="descricao" id="textArea_descricao" rows="6" value={this.state.descricao} onChange={this.onChange} required></textarea>
                                 </div>
 
                                 <br></br>
 
-                                <div class="row">
-                                    <div class="col">
-                                        <label>Preço <span class='text-red'>*</span> </label>
-                                        <input type="number" id='input_preco' class="form-control" required></input>
+                                <div className="row">
+                                    <div className="col">
+                                        <label>Preço <span className='text-red'>*</span> </label>
+                                        <input type="number" id='input_preco' name="preco" className="form-control" value={this.state.preco} onChange={this.onChange} required></input>
                                     </div>
-                                    <div class="col">
-                                        <label>Fornecedor <span class='text-red'>*</span> </label>
-                                        <input type="text" id='input_fornecedor' class="form-control" required></input>
+                                    <div className="col">
+                                        <label>Fornecedor <span className='text-red'>*</span> </label>
+                                        <input type="text" id='input_fornecedor' name="fornecedor" className="form-control" value={this.state.fornecedor} onChange={this.onChange} required></input>
                                     </div>
                                 </div>
 
@@ -78,9 +118,9 @@ class Cadastro extends React.Component {
 
 
                                 <div>
-                                    <button type="button" class="btn btn-success btn_space" onClick={salvar}>Salvar</button>
+                                    <button type="button" className="btn btn-success btn_space" onClick={this.onSubmit} >Salvar</button>
 
-                                    <button type="button" class="btn btn-info" onClick={limpar} >Limpar</button>
+                                    <button type="button" className="btn btn-info" onClick={this.limparCampos} >Limpar</button>
                                 </div>
 
 
